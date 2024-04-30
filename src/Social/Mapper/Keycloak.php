48501d69973003a4b83fe4cf0161b6c27e4e8928
Robitmoh <robit@robitnet.hu>
2024-04-30 15:56:05 +0200

    Add keycloak support
    require stevenmaguire/oauth2-keycloak

---
+++ src/Social/Mapper/Keycloak.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Copyright 2010 - 2019, Cake Development Corporation (https://www.cakedc.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright 2010 - 2019, Cake Development Corporation (https://www.cakedc.com)
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+
+namespace CakeDC\Auth\Social\Mapper;
+use Cake\Core\Configure;
+
+class Keycloak extends AbstractMapper
+{
+    /**
+     * Map for provider fields
+     *
+     * @var array
+     */
+    protected array $_mapFields = [
+        'first_name' => 'given_name',
+        'last_name' => 'family_name',
+        'email' => 'email',
+        'username' => 'preferred_username',
+        'id' => 'sub',
+        'link' => 'website',
+        'roles' => 'Cakedc'
+    ];
+    protected $_rolesMatch = 'CakeDc-';
+
+
+    function _roles($data){   # Client Scopes > roles > Mappers > realm roles -> Add to userinfo  := Enable
+        $roles = array_filter(@$data[$this->_mapFields['roles']]['cakedc'], 
+            function ($var) { return (strpos($var, $this->_rolesMatch) !== false); });
+        
+        $role= str_ireplace($this->_rolesMatch, '',array_pop($roles));
+        #
+        # Set the cakedc default user role from keycloak roles
+        #
+        Configure::write('Users.Registration.defaultRole',$role);
+        Configure::write('Users.Registration.KeycloakRole',$role);
+        return $role;   
+    }
+}
